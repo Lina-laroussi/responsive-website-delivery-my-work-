4191499a43d40447ab6ac2b8e9718eb8a8b93585
Lina-laroussi <88428040+Lina-laroussi@users.noreply.github.com>
2021-09-19 22:59:20 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,57 @@
+/*==================== SHOW MENU ====================
+//on a utilisé la fonction flechée
+const showMenu = (nav_toggle, nav_menu) =>{
+    const toggle = document.getElementById(nav_toggle),
+    nav = document.getElementById(nav_menu)
+    
+    // Validate that variables exist
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            // We add the show-menu class to the div tag with the nav__menu class
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+showMenu('nav_toggle','nav_menu')//appel de la fonction*/
+
+/*==================== REMOVE MENU MOBILE ====================
+const navLink = document.querySelectorAll('.nav_link')//on a selectionné le lien
+
+function linkAction(){
+    const navMenu = document.getElementById('nav_menu')//on a selectionné le menu
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+*/
+/***********change theme************ */
+
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bx-toggle-right'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-toggle-left' : 'bx-toggle-right'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'bx-toggle-left' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
